178455ba217afc0e66b241db18f97e5448f33fd4
josue <jcalderonv05@gmail.com>
2019-10-25 23:41:08 -0600

    Initial version of jhipsterTest generated by JHipster-6.4.1

---
+++ src/main/java/com/cenfotec/jhipster/service/UsernameAlreadyUsedException.java
@@ -0,0 +1,9 @@
+package com.cenfotec.jhipster.service;
+
+public class UsernameAlreadyUsedException extends RuntimeException {
+
+    public UsernameAlreadyUsedException() {
+        super("Login name already used!");
+    }
+
+}
